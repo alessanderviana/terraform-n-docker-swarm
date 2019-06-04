8e34c566233d6d4fac90ed04d6eaee5697114be8
alessanderviana <alessander.viana@hotmail.com>
2019-06-03 23:12:56 -0300

    Changed mkdir line, added some commands as remark

--- salt/salt-master-config.sh
+++ salt/salt-master-config.sh
@@ -6,9 +6,14 @@ sed -i 's/#file_recv: False/file_recv: True/' /etc/salt/master
 sed -i 's/#pillar_roots:/pillar_roots:/' /etc/salt/master
 sed -i 's/#  base:/  base:/' /etc/salt/master
 sed -i 's/#    - \/srv\/pillar/    - \/srv\/pillar/' /etc/salt/master
-mkdir -p /srv/{salt,pillar}
+mkdir -p /srv/pillar
+# mkdir -p /srv/{salt,pillar}
 echo '========== Configuring Salt minion =========='
 sed -i 's/#master: salt/master: 127.0.0.1/' /etc/salt/minion
 echo '========== Restarting services =========='
 systemctl restart salt-master && systemctl restart salt-minion
+
 # tail -f /var/log/syslog | grep startup-script
+# salt-key -L
+# salt-key -A -y
+# salt '*' test.ping
