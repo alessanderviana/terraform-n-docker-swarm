8591b38222854392a2fc2298284c74fd77e74532
alessanderviana <alessander.viana@hotmail.com>
2019-06-04 18:12:59 -0300

    Added some echo commands

--- salt/docker-swarm-init-cluster.sh
+++ salt/docker-swarm-init-cluster.sh
@@ -6,10 +6,12 @@ NODE_KEYS=$( salt-key -l un | grep -c cluster )
 # If is not empty accept the node keys
 if [ $NODE_KEYS -ge 1 ]; then
   salt-key -A -y
+  echo ""
 fi
 
 # Initialize the leader cluster
 docker swarm init
+echo ""
 
 # Get the worker token
 TOKEN=$( docker swarm join-token worker | grep join )
@@ -18,4 +20,5 @@ TOKEN=$( docker swarm join-token worker | grep join )
 SWARM_NODES=$( salt-key -l acc | grep -v Keys | grep -v cluster-1 )
 
 # Join the nodes to the cluster
+echo "Joining nodes ..."
 for I in $( echo $SWARM_NODES ); do salt "$I" cmd.run "${TOKEN}"; done
