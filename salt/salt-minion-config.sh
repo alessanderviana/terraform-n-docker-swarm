9f6f741d57f0a9519838a104363c917083f22f8c
alessanderviana <alessander.viana@hotmail.com>
2019-06-03 23:12:07 -0300

    Added salt-minion-config script

---
+++ salt/salt-minion-config.sh
@@ -0,0 +1,11 @@
+echo '========== Getting credentials and auth =========='
+gsutil cp gs://terraform-n-docker-swarm/kubernetes-svc.json credentials.json
+gcloud auth activate-service-account \
+  kubernetes-svc@infra-como-codigo-e-automacao.iam.gserviceaccount.com \
+  --key-file=credentials.json
+echo '========== Getting master IP =========='
+SALT_MASTER_IP=$( gcloud compute instances list --filter="(name=swarm-cluster-1 AND zone:us-central1-b)" --format="value(networkInterfaces[0].networkIP)" )
+echo '========== Configuring Salt minion =========='
+sed -i 's/#master: salt/master: '$SALT_MASTER_IP'/' /etc/salt/minion
+echo '========== Restarting service =========='
+systemctl restart salt-minion
