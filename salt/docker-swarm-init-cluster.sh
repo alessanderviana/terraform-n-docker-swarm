50e83b8ed09353fb7344a96b1e2fc3b675ffa761
alessanderviana <alessander.viana@hotmail.com>
2019-06-04 18:28:51 -0300

    Added a delay before join nodes

--- salt/docker-swarm-init-cluster.sh
+++ salt/docker-swarm-init-cluster.sh
@@ -19,6 +19,9 @@ TOKEN=$( docker swarm join-token worker | grep join )
 # Get the swarm nodes (no leaders)
 SWARM_NODES=$( salt-key -l acc | grep -v Keys | grep -v cluster-1 )
 
+echo "Waiting 5 seconds ..."
+sleep 5
+
 # Join the nodes to the cluster
 echo "Joining nodes ..."
 for I in $( echo $SWARM_NODES ); do salt "$I" cmd.run "${TOKEN}"; done
